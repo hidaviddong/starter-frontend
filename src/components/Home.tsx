62ac819ab6a8e4c0e224ba61ef5ad3aa2974cf01
david dong <hi@daviddong.me>
2024-01-05 13:57:38 +0800

    fix: grammer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   const [count, setCount] = useAtom(counterAtom)
   return (
     <div className="h-40 w-40">
-      <h1 className="text-bold text-3xl">Zustand</h1>
-      <Button onClick={() => setCount(count + 1)}>点我{count}次</Button>
+      <h1 className="text-bold text-3xl">Jotai</h1>
+      <Button onClick={() => setCount(count + 1)}>Click me {count}</Button>
       <Link to="/about">About</Link>
     </div>
   )
